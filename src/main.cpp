67dcf52331870f80201250bc7905683a40dcc106
MichaelKazarian <28EMACSlisp>
2026-07-16 12:23:20 +0300

    refactor: Move roll motor updates to AUTO_WAIT_LIMITS state

--- src/main.cpp
+++ src/main.cpp
@@ -525,9 +525,6 @@ void runAutoMode() {
   bool cycleStartEdge = cycleStartPressed && !prevCycleStartPressed;
   prevCycleStartPressed = cycleStartPressed;
 
-  updateMotorSpeed(MOTOR_ROLL1, adcRead(ADC_CH_19));
-  updateMotorSpeed(MOTOR_ROLL2, adcRead(ADC_CH_22));
-
   switch (state) {
   case AUTO_WAIT_START:
     raiseCylinders();
@@ -552,6 +549,8 @@ void runAutoMode() {
     break;
 
   case AUTO_WAIT_LIMITS:
+    updateMotorSpeed(MOTOR_ROLL1, adcRead(ADC_CH_19));
+    updateMotorSpeed(MOTOR_ROLL2, adcRead(ADC_CH_22));
     if (handleCylinderRetraction()) {
       state = AUTO_WAIT_START;
     }
